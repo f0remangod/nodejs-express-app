8f2a082140e6fdbb62a3f5da0ce6852f0093b638
Viktor Biletskyi <106250265+f0remangod@users.noreply.github.com>
2023-03-20 17:22:07 +0200

    added CRUD for MangoDB data, error handlers, validators, PATCH route

---
+++ models/contacts/contact.js
@@ -0,0 +1,11 @@
+const { model } = require("mongoose");
+
+const { mongooseContactSchema } = require("../../validation/contacts");
+
+const { handleSchemaValidationError } = require("../../helpers");
+
+mongooseContactSchema.post("save", handleSchemaValidationError);
+
+const Contact = model("contact", mongooseContactSchema);
+
+module.exports = Contact;
